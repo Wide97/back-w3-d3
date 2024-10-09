0319a41445efe4d39bf1f94df069eabb3207e31b
Wide97 <mwidesott@gmail.com>
2024-10-09 15:59:21 +0200

    init

---
+++ src/main/java/marcowidesott/Application.java
@@ -0,0 +1,8 @@
+package marcowidesott;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/marcowidesott/ApplicationTest.java
@@ -0,0 +1,13 @@
+package marcowidesott;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
