04c957e062401a71320cce900579f7ab84c77aaa
Wide97 <mwidesott@gmail.com>
2024-10-09 16:06:38 +0200

    init

--- src/main/java/marcowidesott/Application.java
+++ src/main/java/marcowidesott/Application.java
@@ -1,8 +1,13 @@
 package marcowidesott;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("backd2w3");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hello!");
+
     }
 }
